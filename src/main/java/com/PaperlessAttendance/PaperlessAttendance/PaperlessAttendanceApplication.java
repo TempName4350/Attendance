94658d3d7084bd358fa4ab69bcd5166f360b24ac
ashleyswinning <ash.huynh@uga.edu>
2020-03-01 21:45:00 -0500

    Added in initial configuration for front end and back end of Spring Boot + Angular app

---
+++ src/main/java/com/PaperlessAttendance/PaperlessAttendance/PaperlessAttendanceApplication.java
@@ -0,0 +1,30 @@
+package com.PaperlessAttendance.PaperlessAttendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.PaperlessAttendance.PaperlessAttendance.entities.User;
+import com.PaperlessAttendance.PaperlessAttendance.repositories.UserRepository;
+import java.util.stream.Stream;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class PaperlessAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperlessAttendanceApplication.class, args);
+	}
+
+    @Bean
+    CommandLineRunner init(UserRepository userRepository) {
+        return args -> {
+            Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
+                User user = new User(name, name.toLowerCase() + "@domain.com");
+                userRepository.save(user);
+            });
+            userRepository.findAll().forEach(System.out::println);
+        };
+    }
+}
---
+++ src/test/java/com/PaperlessAttendance/PaperlessAttendance/PaperlessAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PaperlessAttendance.PaperlessAttendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperlessAttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
