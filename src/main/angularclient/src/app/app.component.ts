d83f2f00a724e12109cd835c2c99790a7c92443f
TempName4350 <60479090+TempName4350@users.noreply.github.com>
2020-04-02 16:08:36 -0400

    Revert "CommitV1"

--- src/main/angularclient/src/app/app.component.ts
+++ src/main/angularclient/src/app/app.component.ts
@@ -1,35 +1,15 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-
-import { AuthenticationService } from './_services';
-
-
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-
+ 
   title: string;
-  currentUser: any;
-
-  constructor(
-    private router: Router,
-    private authenticationService: AuthenticationService
-
-  ) {
-
+ 
+  constructor() {
     this.title = 'Paperless Attendance';
-    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
-
   }
-  logout() {
-    this.authenticationService.logout();
-    this.router.navigate(['/login']);
-  }
-
 }
-
-
--- src/main/angularclient/src/app/app.module.ts
+++ src/main/angularclient/src/app/app.module.ts
@@ -1,46 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRouting } from './app.routing';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { StudentServiceService } from './service/student-service.service';
 import { UploadStudentsComponent } from './upload-students/upload-students.component';
 import { ExportStudentsComponent } from './export-students/export-students.component';
-import { LoginComponent } from './login';
-import {ErrorInterceptor, fakeBackendProvider, JwtInterceptor} from './_helpers';
-import {HomeComponent} from './home';
-
-
-
-
+ 
 @NgModule({
   declarations: [
     AppComponent,
     UserListComponent,
     UploadStudentsComponent,
-    ExportStudentsComponent,
-    LoginComponent,
-    HomeComponent
-
+    ExportStudentsComponent
   ],
   imports: [
     BrowserModule,
-    AppRouting,
+    AppRoutingModule,
     HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule
+    FormsModule
   ],
-  providers: [
-
-    StudentServiceService,
-    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-
-    fakeBackendProvider ],
-
-
+  providers: [StudentServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
