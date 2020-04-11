5afe1ea66ba065233d8d31d587e1b52af9cdd9d2
Christopher White <cwhite88@student.gsu.edu>
2020-04-11 15:49:45 -0400

    CommitV2 with register

--- src/main/angularclient/src/app/user-list/user-list.component.ts
+++ src/main/angularclient/src/app/user-list/user-list.component.ts
@@ -1,35 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { Student } from '../model/student';
 import { StudentServiceService } from '../service/student-service.service';
-import {Router} from "@angular/router"
-import {ActivatedRoute} from "@angular/router"
+import {Router} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
- 
+
   students: Student[];
-  showSuccessMsg: boolean = false;
-  showErrorMsg: boolean = false;
- 
+  showSuccessMsg = false;
+  showErrorMsg = false;
+
   constructor(private studentService: StudentServiceService, private router: Router, private route: ActivatedRoute) {
   }
- 
+
   ngOnInit() {
     console.log(this.route);
 
-    if(this.route.snapshot.params.uploadSuccess !== undefined && this.route.snapshot.params.uploadSuccess === 'true') {
+    if (this.route.snapshot.params.uploadSuccess !== undefined && this.route.snapshot.params.uploadSuccess === 'true') {
       this.showSuccessMsg = true;
       this.showErrorMsg = false;
     }
 
-    if(this.route.snapshot.params.uploadFailure !== undefined && this.route.snapshot.params.uploadFailure === 'true') {
+    if (this.route.snapshot.params.uploadFailure !== undefined && this.route.snapshot.params.uploadFailure === 'true') {
       this.showErrorMsg = true;
       this.showSuccessMsg = false;
     }
-  
+
     this.studentService.findAll().subscribe(data => {
       this.students = data;
     });
