0daf9fddac9ba1456295f9dafb9cf1e9e0abc1ec
ash-huy <ahuynh12@student.gsu.edu>
2020-04-15 17:02:57 -0400

    updated copy and removed some commented code and redundancy

--- src/main/angularclient/src/app/attendance/attendance.component.ts
+++ src/main/angularclient/src/app/attendance/attendance.component.ts
@@ -10,13 +10,14 @@ import { HttpClient, HttpHeaders, HttpParams, HttpErrorResponse} from '@angular/
 export class AttendanceComponent implements OnInit {
     dateAttend: DateAttend[];
     dateIDs: number[];
-    hey: {}
+    currentDate: string;
 
     constructor(public datepipe: DatePipe, private http: HttpClient, private dateAttendService: DateAttendServiceService, private router: Router, private route: ActivatedRoute) {
     }
 
     ngOnInit(): void {
-
+      let todaysDate = new Date();
+      this.currentDate = this.datepipe.transform(todaysDate, 'yyyy-MM-dd');
     }
 
     startSession(): void {
@@ -24,8 +25,6 @@ export class AttendanceComponent implements OnInit {
             // get all dates
             this.dateAttend = data;
             let maxDateID = 0;
-            let todaysDate = new Date();
-            let date = this.datepipe.transform(todaysDate, 'yyyy-MM-dd');
     
             this.dateAttend.forEach(element => {
                 if (element.dateID > maxDateID) {
@@ -36,12 +35,12 @@ export class AttendanceComponent implements OnInit {
             let newDateAttendPresent = new DateAttend();
             newDateAttendPresent.dateID = maxDateID + 1;
             newDateAttendPresent.attended = "yes";
-            newDateAttendPresent.date = date;
+            newDateAttendPresent.date = this.currentDate;
     
             let newDateAttendAbsent = new DateAttend();
             newDateAttendAbsent.dateID = newDateAttendPresent.dateID + 1;
             newDateAttendAbsent.attended = "no";
-            newDateAttendAbsent.date = date;
+            newDateAttendAbsent.date = this.currentDate;
     
             this.dateAttendService.save(newDateAttendPresent).subscribe(data => {
             });
